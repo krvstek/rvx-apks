90e3b8f2da49fbc0b503c48a29fcd3ca6371f07e
krvstek. <138402769+krvstek@users.noreply.github.com>
2025-10-09 16:37:18 +0000

    update: deps

--- utils.sh
+++ utils.sh
@@ -204,7 +204,7 @@ _req() {
 		mv -f "$dlp" "$op"
 	fi
 }
-req() { _req "$1" "$2" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:135.0) Gecko/20100101 Firefox/135.0"; }
+req() { _req "$1" "$2" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:142.0) Gecko/20100101 Firefox/142.0"; }
 gh_req() { _req "$1" "$2" -H "$GH_HEADER"; }
 gh_dl() {
 	if [ ! -f "$1" ]; then
